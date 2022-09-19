31c1ce148ce65910ccc6e2789c866da3e7c85ce8
joelcaminas02 <joelcg2002@gmail.com>
2022-09-19 12:06:39 +0200

    nada

--- Practica2/ejercicio1.js
+++ Practica2/ejercicio1.js
@@ -2,4 +2,5 @@ var precio = parseFloat(prompt('Introduce el precio del producto'));
 
 var iva = precio*0.21;
 var total = precio+iva;
-alert('El iva del procucto es '+iva.toFixed(2)+" y el total serian: "+total.toFixed(2));
+alert('El iva del procucto es '+iva.toFixed(2)+" y el total serian: "+total.toFixed(2));ar opcion = window.confirm();
+    
