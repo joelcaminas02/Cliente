5c5e73fa6acca7da003b49b0b99bf2cb3216b865
joelcaminas02 <joelcg2002@gmail.com>
2022-10-04 12:04:13 +0200

    todo

--- Practica 3/ejercicio2.js
+++ Practica 3/ejercicio2.js
@@ -1,5 +1,13 @@
-function enviar(){
-    if(document.formularioDni.dni.value.length() == 0){
-        alert('No hat parametros')
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+    });
+    function validarFormulario(evento) {
+    evento.preventDefault();
+    var clave = document.getElementById('dni').value;
+    if (clave.length == 9) {
+    alert('La clave es válida');
+    }else{
+    alert('La clave NO es válida');
     }
-}
+    this.submit();
+    }
